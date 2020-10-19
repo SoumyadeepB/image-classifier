1cf8eadf34b9e5b2553a581cdfe0128653d49005
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2020-10-19 14:30:43 +0200

    Updates

---
+++ precache-manifest.cf440a653b4393013cc08a79fd05c9cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71a6725f1e11738156b4e332297063f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e9dbcc3791818fb2c2e",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1866be21b4b228e33af7",
+    "url": "/static/css/main.d921e1d0.chunk.css"
+  },
+  {
+    "revision": "5e9dbcc3791818fb2c2e",
+    "url": "/static/js/2.0f6717c4.chunk.js"
+  },
+  {
+    "revision": "3dff47cb1e3a6c84319037ed0950bb82",
+    "url": "/static/js/2.0f6717c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1866be21b4b228e33af7",
+    "url": "/static/js/main.cd283b8b.chunk.js"
+  },
+  {
+    "revision": "dbc3ceb7972a6d6cf39c",
+    "url": "/static/js/runtime-main.9a01228d.js"
+  }
+]);
