8d709efa1330cb43c7a7b276b30d91ec915761cd
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2020-10-19 23:25:43 +0200

    Updates

--- precache-manifest.0df64595d618366765384dcf1e9d6268.js
+++ precache-manifest.0df64595d618366765384dcf1e9d6268.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88f688e9a71d1ae2df2ac140753f6ff3",
+    "revision": "c5edfb9a5856f814a025b2c4324afe2c",
     "url": "/image-classifier/index.html"
   },
   {
-    "revision": "0a7934f7b129df990843",
+    "revision": "cd4245aed4af6ac7866c",
     "url": "/image-classifier/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "4d459420ddc26b273894",
+    "revision": "b9c3bd2dd8cbda0d4b5b",
     "url": "/image-classifier/static/css/main.d921e1d0.chunk.css"
   },
   {
-    "revision": "0a7934f7b129df990843",
-    "url": "/image-classifier/static/js/2.5f7898cf.chunk.js"
+    "revision": "cd4245aed4af6ac7866c",
+    "url": "/image-classifier/static/js/2.36d43e36.chunk.js"
   },
   {
     "revision": "3dff47cb1e3a6c84319037ed0950bb82",
-    "url": "/image-classifier/static/js/2.5f7898cf.chunk.js.LICENSE.txt"
+    "url": "/image-classifier/static/js/2.36d43e36.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4d459420ddc26b273894",
-    "url": "/image-classifier/static/js/main.4a71148f.chunk.js"
+    "revision": "b9c3bd2dd8cbda0d4b5b",
+    "url": "/image-classifier/static/js/main.ccbf559f.chunk.js"
   },
   {
     "revision": "0ecbcd73cd7361a110d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/image-classifier/precache-manifest.e9065a6b2a9abe4f55d02defa5e2f02d.js"
+  "/image-classifier/precache-manifest.0df64595d618366765384dcf1e9d6268.js"
 );
 
 self.addEventListener('message', (event) => {
