6dd33804b1245c8d2820b1e349da03717ab2370b
SoumyadeepB <soumyadeep.bh1994@gmail.com>
2021-05-08 22:51:53 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/image-classifier/precache-manifest.8154f2f05a67ffeba81e8079dca46756.js"
+  "/image-classifier/precache-manifest.629713875cbc0767a2653ef79f031478.js"
 );
 
 self.addEventListener('message', (event) => {
